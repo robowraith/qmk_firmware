a382d350bdad371a52578e85f6d68b13fe6489ca
Joachim Hoss <robowraith@gmail.com>
2021-11-04 00:05:45 +0100

    Remove Macos layers

--- keyboards/momoka_ergo/keymaps/robowraith/keymap.c
+++ keyboards/momoka_ergo/keymaps/robowraith/keymap.c
@@ -44,17 +44,6 @@ void keyboard_post_init_user(void)
 #define BO_MO5R LT(5, KC_ENT)
 */
 
-// Defines names for use in layer keycodes and the keymap
-/*
-enum layer_names {
-    _BONEL,
-    _BONEM,
-    _SLL,
-    _SLM,
-    _UTIL
-};
-*/
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Bone for Linux */
     [0] = LAYOUT(
@@ -62,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   DE_J,    DE_D,    DE_U,    DE_A,    DE_X,                                           DE_P,    DE_H,    DE_L,    DE_M,    DE_W,    DE_SS,
         KC_CAPS,  BO_C,    BO_T,    BO_I,    BO_E,    DE_O,                                           DE_B,    BO_N,    BO_R,    BO_S,    BO_G,    DE_Q,
         KC_LSFT,  DE_F,    DE_V,    DE_UDIA, DE_ADIA, DE_ODIA,                                        DE_Y,    DE_Z,    DE_COMM, DE_DOT,  DE_K,    KC_RSFT,
-        TT(5),    KC_GRV,  KC_EQL,  MO(5),   MO(1),                                                            MO(1),   MO(5),   KC_LBRC, KC_RBRC, TT(5),
+        TT(3),    KC_GRV,  KC_EQL,  MO(3),   MO(1),                                                            MO(1),   MO(3),   KC_LBRC, KC_RBRC, TT(3),
                                                                KC_LCTL, KC_LALT,    KC_RGUI, KC_RCTL,
                                                                         KC_HOME,    KC_PGUP,
                                                       KC_BSPC, KC_DEL,  KC_END,     KC_PGDN, KC_ENT,  KC_SPC
@@ -79,35 +68,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                       C(DE_C), C(DE_V), KC_END,     KC_PGDN, C(DE_V),  C(DE_C)
 
     ),
-    /* Bone for Mac */
-    [2] = LAYOUT(
-        KC_ESC,   DE_1,    DE_2,    DE_3,    DE_4,    DE_5,                                           DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    KC_BSPC,
-        KC_TAB,   DE_J,    DE_D,    DE_U,    DE_A,    DE_X,                                           DE_P,    DE_H,    DE_L,    DE_M,    DE_W,    DE_SS,
-        KC_CAPS,  BO_C,    BO_T,    BO_I,    BO_E,    DE_O,                                           DE_B,    BO_N,    BO_R,    BO_S,    BO_G,    DE_Q,
-        KC_LSFT,  DE_F,    DE_V,    DE_UDIA, DE_ADIA, DE_ODIA,                                        DE_Y,    DE_Z,    DE_COMM, DE_DOT,  DE_K,    KC_RSFT,
-        TT(5),    KC_GRV,  KC_EQL,  MO(5),   MO(3),                                                            MO(3),   MO(5),   KC_LBRC, KC_RBRC, TT(5),
-                                                               KC_LCTL, KC_LALT,    KC_RGUI, KC_RCTL,
-                                                                        KC_HOME,    KC_PGUP,
-                                                      KC_BSPC, KC_DEL,  KC_END,     KC_PGDN, KC_ENT,  KC_SPC
-    ),
-    /* Shifted Layer for Mac */
-    [3] = LAYOUT(
-        KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                          KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,
-        KC_NO,    DE_DEG,  DE_UNDS, A(DE_5), A(DE_6), DE_CIRC,                                        DE_EXLM, DE_LABK, DE_RABK,  DE_EQL,  DE_AMPR, DE_EURO,
-        KC_NO,    LSA(DE_7),DE_SLSH,A(DE_8), A(DE_9), DE_ASTR,                                        DE_QUES, DE_LPRN, DE_RPRN,  DE_MINS, DE_COLN, DE_AT,
-        KC_NO,    DE_HASH, DE_DLR,  A(DE_7), A(DE_N), DE_GRV,                                         DE_PLUS, DE_PERC, DE_DQUO,  DE_QUOT, DE_SCLN, KC_NO,
-        KC_NO,    KC_NO,   KC_NO,   KC_NO,   G(DE_X),                                                          G(DE_X), KC_NO,    KC_NO,   KC_NO,   KC_NO,
-                                                               KC_LCTL, KC_LALT,    KC_RGUI, KC_RCTL,
-                                                                        KC_HOME,    KC_PGUP,
-                                                      G(DE_C), G(DE_V), KC_END,     KC_PGDN, G(DE_V),  G(DE_C)
-    ),
     /* Gaming */
-    [4] = LAYOUT(
+    [2] = LAYOUT(
         KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        MO(5),    KC_GRV,  KC_EQL,  KC_LEFT, KC_RGHT,                                                          KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(5),
+        MO(3),    KC_GRV,  KC_EQL,  KC_LEFT, KC_RGHT,                                                          KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(3),
                                                                KC_LCTL, KC_LALT,    KC_RGUI, KC_RCTL,
                                                                         KC_HOME,    KC_PGUP,
                                                       KC_BSPC, KC_DEL,  KC_END,     KC_PGDN, KC_ENT,  KC_SPC
@@ -120,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RESET,    _______, _______, KC_MPRV, KC_MNXT, KC_MPLY,                                        KC_TAB, _______, _______, _______, _______, RESET,
         KC_TRNS,  _______, KC_MUTE, KC_VOLD, KC_VOLU,                                                          _______, _______, _______, _______, KC_TRNS,
                                                                _______, KC_PSCR,    TG(2),   RGB_TOG,
-                                                                        _______,    TG(4),
+                                                                        _______,    _______,
                                                       KC_BSPC, KC_DEL,  KC_ESC,     KC_TAB,  KC_ENT,  KC_SPC
     )
 };
@@ -137,19 +104,11 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             rgblight_sethsv_noeeprom(HSV_CYAN);
             rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
             break;
-        case 2: // Bone (Mac)
-            rgblight_sethsv_noeeprom(HSV_RED);
-            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-            break;
-        case 3: // Shifted (Mac)
-            rgblight_sethsv_noeeprom(HSV_ORANGE);
-            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-            break;
-        case 4: // Gaming
+        case 2: // Gaming
             rgblight_sethsv_noeeprom(HSV_BLUE);
             rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_SWIRL + 5);
             break;
-        case 5: // Utility
+        case 3: // Utility
             rgblight_sethsv_noeeprom(HSV_WHITE);
             rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
             break;

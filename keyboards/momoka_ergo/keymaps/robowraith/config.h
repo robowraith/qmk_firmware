6c8eddec1221c0cbadd6c2aa22c009ae2e348e66
Joachim Hoss <joachim.hoss@42he.com>
2022-03-22 13:48:04 +0100

    Set useful tapping terms

--- keyboards/momoka_ergo/keymaps/robowraith/config.h
+++ keyboards/momoka_ergo/keymaps/robowraith/config.h
@@ -66,6 +66,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Set Tapping Toggle */
 #define TAPPING_TOGGLE 2
 
+// default but used in macros
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
 /* Enable lighting layers */
 #define RGBLIGHT_LAYERS
 #define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF

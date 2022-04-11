1838cde07d0619fc9a7b588aa7c9da470e7acc15
Joachim Hoss <joachim.hoss@42he.com>
2022-04-11 12:58:39 +0200

    Many changes

--- keyboards/xbows/nature/keymaps/robowraith/keymap.c
+++ keyboards/xbows/nature/keymaps/robowraith/keymap.c
@@ -15,7 +15,6 @@
  */
 #include QMK_KEYBOARD_H
 #include "keymap_german.h"
-//#include "rgblight_list.h"
 
 // Home-Row-Mod-Keys for QWERTZ and Bone
 #define BO_C LGUI_T(DE_C)
@@ -58,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_COPY,    DE_J,       DE_D,       DE_U,       DE_A,       DE_X,                   DE_P,       DE_H,       DE_L,       DE_M,       DE_W,        DE_SS,     DE_PLUS,    DE_HASH,    KC_PGUP,
       KC_PASTE,   BO_C,       BO_T,       BO_I,       BO_E,       BO_MO1L,    KC_ESC,     BO_MO1R,    BO_N,       BO_R,       BO_S,       BO_G,        DE_Q,      KC_ENT,                 KC_PGDN,
       KC_CAPS,    DE_F,       DE_V,       DE_UDIA,    BO_MO2L,    DE_ODIA,    KC_TAB,     DE_Y,       BO_MO2R,    DE_COMM,    DE_DOT,     DE_K,        KC_RSFT,               KC_UP,
-      KC_LCTL,    KC_LGUI,    KC_LALT,    KC_BSPC,                KC_DEL,                 KC_ENT,                 KC_SPC,     KC_RALT,    TT(3),       KC_RCTL,   KC_LEFT,    KC_DOWN,    KC_RGHT),
+      KC_LCTL,    KC_LGUI,    KC_LALT,    KC_BSPC,                KC_DEL,                 KC_ENT,                 KC_SPC,     KC_RALT,    TG(3),       KC_RCTL,   KC_LEFT,    KC_DOWN,    KC_RGHT),
   [1] = LAYOUT(
       // Shifted Layer Linux
       KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
@@ -66,15 +65,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_NO,      DE_DEG,     DE_UNDS,    DE_LBRC,    DE_RBRC,    DE_CIRC,                DE_EXLM,    DE_LABK,    DE_RABK,    DE_EQL,     DE_AMPR,    DE_EURO,    KC_NO,      KC_NO,      KC_NO,
       KC_NO,      DE_BSLS,    DE_SLSH,    DE_LCBR,    DE_RCBR,    DE_ASTR,    KC_NO,      DE_QUES,    DE_LPRN,    DE_RPRN,    DE_MINS,    DE_COLN,    DE_AT,      KC_NO,                  KC_NO,
       KC_NO,      DE_HASH,    DE_DLR,     DE_PIPE,    DE_TILD,    DE_GRV,     KC_NO,      DE_PLUS,    DE_PERC,    DE_DQUO,    DE_QUOT,    DE_SCLN,    KC_NO,                  KC_NO,
-      KC_NO,      KC_NO,      C(DE_X),    C(DE_C),                S(KC_INS),              S(KC_INS),              C(DE_C),    C(DE_X),    MO(5),      KC_NO,      KC_NO,      KC_NO,      KC_NO),
+      KC_NO,      KC_NO,      C(DE_X),    C(DE_C),                S(KC_INS),              S(KC_INS),              C(DE_C),    C(DE_X),    MO(2),      KC_NO,      KC_NO,      KC_NO,      KC_NO),
   [2] = LAYOUT(
       // Utillity
       KC_ESC,     KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      RGB_TOG,    NK_TOGG,    EEP_RST,
       KC_CIRC,    DE_1,       DE_2,       DE_3,       DE_4,       DE_5,                   DE_6,       DE_7,       DE_8,       DE_9,       DE_0,       KC_NO,      KC_NO,      RESET,
-      KC_TAB,     KC_NO,      KC_HOME,    KC_UP,      KC_END,     KC_PGUP,                KC_NLCK,    KC_KP_7,    KC_KP_8,    KC_KP_9,    KC_KP_PLUS, KC_PAST,    KC_NO,      KC_NO,      KC_HOME,
-      KC_NO,      KC_LSFT,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_PGDN,    KC_NO,      KC_NO,      KC_KP_4,    KC_KP_5,    KC_KP_6,    KC_KP_MINUS,KC_PSLS,    KC_ENTER,               KC_END,
-      KC_NO,      KC_LCTL,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_KP_1,    KC_KP_2,    KC_KP_3,    KC_NO,      KC_MUTE,                KC_VOLU,
-      KC_NO,      KC_NO,      KC_NO,      KC_LCTRL,               KC_LGUI,                KC_LSFT,                KC_KP_0,    DE_COMM,    KC_TRNS,    KC_MPLY,    KC_MPRV,    KC_VOLD,    KC_MNXT),
+      DT_PRNT,    KC_NO,      KC_HOME,    KC_UP,      KC_END,     KC_PGUP,                KC_NLCK,    KC_KP_7,    KC_KP_8,    KC_KP_9,    KC_KP_PLUS, KC_PAST,    KC_NO,      KC_NO,      KC_HOME,
+      DT_UP,      KC_LSFT,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_PGDN,    KC_NO,      KC_NO,      KC_KP_4,    KC_KP_5,    KC_KP_6,    KC_KP_MINUS,KC_PSLS,    KC_ENTER,               KC_END,
+      DT_DOWN,    KC_LCTL,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_KP_1,    KC_KP_2,    KC_KP_3,    KC_NO,      KC_MUTE,                KC_VOLU,
+      KC_NO,      KC_NO,      KC_NO,      KC_LCTRL,               KC_LGUI,                KC_LSFT,                KC_KP_0,    DE_COMM,    TG(0),      KC_MPLY,    KC_MPRV,    KC_VOLD,    KC_MNXT),
+  [3] = LAYOUT(
+      // Gaming (QWERTY)
+      KC_ESC,     KC_F1,      KC_F2,     KC_F3,       KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,     KC_PSCR,
+	  KC_GRV,     KC_1,       KC_2,      KC_3,        KC_4,       KC_5,                   KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
+	  KC_TAB,     KC_Q,       KC_W,      KC_E,        KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_PGUP,
+	  KC_CAPS,    KC_A,       KC_S,      KC_D,        KC_F,       KC_G,       KC_BSPC,    KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT,     KC_PGDN,
+	  KC_LSFT,    KC_Z,       KC_X,      KC_C,        KC_V,       KC_B,       KC_ENT,     KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,    KC_UP,
+	  KC_LCTL,    KC_LGUI,    KC_LALT,                KC_SPC,     KC_LCTL,                KC_LSFT,    KC_SPC,                 KC_RALT,    TG(0),      KC_RCTL,    KC_LEFT,    KC_DOWN,    KC_RGHT),
       };
 
 const int FkeysSize = 12;
@@ -136,7 +143,12 @@ void rgb_matrix_indicators_user(void) {
         case 0: // Bone (Linux)
             rgb_matrix_set_color_all(RGB_WHITE);
             default_color_scheme();
-            rgb_matrix_set_color(logoKey, RGB_BLUE );
+            //rgb_matrix_set_color(logoKey, RGB_WHITE);
+            if (host_keyboard_led_state().caps_lock) {
+                rgb_matrix_set_color(logoKey, RGB_MAGENTA);
+            } else {
+                rgb_matrix_set_color(logoKey, RGB_WHITE);
+            }
             break;
         case 1: // Shifted (Linux)
             rgb_matrix_set_color_all(RGB_BLACK);
@@ -145,7 +157,7 @@ void rgb_matrix_indicators_user(void) {
             set_key_color(CutkeysSize, keysCutkeys, RGB_RED);
             set_key_color(CopykeysSize, keysCopykeys, RGB_GREEN);
             set_key_color(PastekeysSize, keysPastekeys, RGB_YELLOW);
-            rgb_matrix_set_color(logoKey, RGB_CYAN );
+            rgb_matrix_set_color(logoKey, RGB_GREEN );
             break;
         case 2: // Utility
             rgb_matrix_set_color_all(RGB_BLACK);
@@ -167,7 +179,15 @@ void rgb_matrix_indicators_user(void) {
             set_key_color(MMMinuskeysSize, keysMMMinuskeys, RGB_RED);
             rgb_matrix_set_color(77, RGB_YELLOW );
             rgb_matrix_set_color(78, RGB_YELLOW );
-            rgb_matrix_set_color(logoKey, RGB_WHITE );
-           break;
+            rgb_matrix_set_color(logoKey, RGB_RED );
+            break;
+        case 3: // Gaming
+            rgb_matrix_set_color_all(RGB_WHITE);
+            if (host_keyboard_led_state().caps_lock) {
+                rgb_matrix_set_color(logoKey, RGB_MAGENTA);
+            } else {
+                rgb_matrix_set_color(logoKey, RGB_BLUE);
+            }
+            break;
     }
 };

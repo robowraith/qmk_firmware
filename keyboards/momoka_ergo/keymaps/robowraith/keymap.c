ec9d74a9ddb26f1819ae9769bb55c3c5cb827d5a
Joachim Hoss <joachim.hoss@koeln.de>
2022-06-16 14:54:10 +0200

    plit Utility Layer into two again

--- keyboards/momoka_ergo/keymaps/robowraith/keymap.c
+++ keyboards/momoka_ergo/keymaps/robowraith/keymap.c
@@ -29,13 +29,15 @@ enum custom_keycodes {
     BO_S = RALT_T(DE_S),
     BO_G = RGUI_T(DE_G),
     SELWRD = SAFE_RANGE,
-    LTOSL
+    LTOSLR,
+    LTOSLL
 };
 
 enum layer_names {
     _BONE,
     _SHIFTED,
-    _UTILITY,
+    _UTILITY_RIGHT,
+    _UTILITY_LEFT,
     _GAMING,
 };
 
@@ -45,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_COPY,  DE_J,       DE_D,      DE_U,      DE_A,      DE_X,            /**/         DE_P,      DE_H,      DE_L,      DE_M,       DE_W,      DE_SS,
         KC_PASTE, BO_C,       BO_T,      BO_I,      BO_E,      DE_O,            /**/         DE_B,      BO_N,      BO_R,      BO_S,       BO_G,      DE_Q,
         CAPSWRD,  DE_F,       DE_V,      DE_UDIA,   DE_ADIA,   DE_ODIA,         /**/         DE_Y,      DE_Z,      DE_COMM,   DE_DOT,     DE_K,      KC_NO,
-        SELWRD,   KC_BRID,    KC_BRIU,   KC_NO,     LTOSL,                      /**/                    LTOSL,     KC_NO,     KC_VOLD,    KC_VOLU,   KC_MUTE,
+        SELWRD,   KC_BRID,    KC_BRIU,   MO(3),     LTOSLR,                     /**/                    LTOSLL,    MO(2),     KC_VOLD,    KC_VOLU,   KC_MUTE,
                                                                KC_NO,   KC_PSCR,/**/KC_RGUI, KC_NO,
                                                                         KC_NO,  /**/KC_APP,
                                                     KC_BSPC,   KC_DEL,  KC_ESC, /**/KC_TAB,  KC_ENT,    KC_SPC
@@ -60,15 +62,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                         KC_HOME,/**/KC_PGUP,
                                                     KC_BSPC,   KC_DEL,  KC_ESC, /**/KC_TAB,  KC_ENT,    KC_SPC
     ),
-    [_UTILITY] = LAYOUT(
+    [_UTILITY_RIGHT] = LAYOUT(
         KC_NO,    KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      KC_NO,
-        KC_NO,    KC_NO,      KC_HOME,   KC_UP,     KC_END,    KC_PGUP,         /**/         KC_NUM,    KC_KP_7,   KC_KP_8,   KC_KP_9,    KC_PPLS,    KC_PAST,
-        KC_CAPS,  KC_NO,      KC_LEFT,   KC_DOWN,   KC_RIGHT,  KC_PGDN,         /**/         KC_NO,     KC_KP_4,   KC_KP_5,   KC_KP_6,    KC_PMNS,    KC_PSLS,
-        RESET,    KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_KP_1,   KC_KP_2,   KC_KP_3,    KC_NO,      RESET,
+        KC_NO,    KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NUM,    KC_KP_7,   KC_KP_8,   KC_KP_9,    KC_PPLS,    KC_PAST,
+        KC_NO,    BO_C,       BO_T,      BO_I,      BO_E,      KC_NO,           /**/         KC_NO,     KC_KP_4,   KC_KP_5,   KC_KP_6,    KC_PMNS,    KC_PSLS,
+        KC_NO,    KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_KP_1,   KC_KP_2,   KC_KP_3,    KC_NO,      RESET,
         TO(0),    KC_NO,      KC_NO,     KC_NO,     KC_NO,                      /**/                    KC_SPC,    KC_NO,     KC_MPRV,    KC_MNXT,    KC_MPLY,
                                                                KC_NO,   KC_NO,  /**/KC_NO,   KC_NO,
                                                                         KC_NO,  /**/KC_NO,
-                                                    KC_BSPC,   KC_DEL,  KC_NO,  /**/KC_NO,   KC_ENT,    KC_KP_0
+                                                    KC_NO,     KC_NO,   KC_NO,  /**/KC_NO,   KC_ENT,    KC_KP_0
+    ),
+    [_UTILITY_LEFT] = LAYOUT(
+        KC_NO,    KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      KC_NO,
+        KC_NO,    KC_NO,      KC_HOME,   KC_UP,     KC_END,    KC_PGUP,         /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      KC_NO,
+        KC_CAPS,  KC_NO,      KC_LEFT,   KC_DOWN,   KC_RIGHT,  KC_PGDN,         /**/         KC_NO,     BO_N,      BO_R,      BO_S,       BO_G,       KC_NO,
+        RESET,    KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      KC_NO,
+        TO(0),    KC_NO,      KC_NO,     KC_NO,     KC_NO,                      /**/                    KC_NO,     KC_NO,     KC_NO,      KC_NO,      KC_NO,
+                                                               KC_NO,   KC_NO,  /**/KC_NO,   KC_NO,
+                                                                        KC_NO,  /**/KC_NO,
+                                                    KC_BSPC,   KC_DEL,  KC_NO,  /**/KC_NO,   KC_NO,     KC_NO
     ),
     [_GAMING] = LAYOUT(
         KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,            /**/         KC_F1,     KC_F2,     KC_F3,     KC_F4,      KC_F5,      KC_F6,
@@ -83,24 +95,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 static uint8_t ltosl_state = 0;
-#define LTOSL_MO_LAYER 2   // Layer to activate when holding.
-#define LTOSL_OSL_LAYER 1  // Layer to activate as an OSL when tapped.
+#define LTOSLR_MO_LAYER 2   // Layer to activate when holding.
+#define LTOSLR_OSL_LAYER 1  // Layer to activate as an OSL when tapped.
+#define LTOSLL_MO_LAYER 3   // Layer to activate when holding.
+#define LTOSLL_OSL_LAYER 1  // Layer to activate as an OSL when tapped.
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-  if (keycode == LTOSL) {
+  if (keycode == LTOSLR) {
     static uint32_t tap_deadline = 0;
     if (record->event.pressed) {  // On pressed.
       tap_deadline = timer_read32() + 200;  // Set 200 ms tap deadline.
-      layer_on(LTOSL_MO_LAYER);
+      layer_on(LTOSLR_MO_LAYER);
       ltosl_state = 1;  // Set undetermined state.
     } else {  // On release.
-      layer_off(LTOSL_MO_LAYER);
+      layer_off(LTOSLR_MO_LAYER);
       if (ltosl_state && !timer_expired32(timer_read32(), tap_deadline)) {
-        // LTOSL was released without pressing another key within 200 ms.
-        layer_on(LTOSL_OSL_LAYER);
+        // LTOSLR was released without pressing another key within 200 ms.
+        layer_on(LTOSLR_OSL_LAYER);
         ltosl_state = 2;  // Acting like OSL.
       }
     }
     return false;
+  }
+  if (keycode == LTOSLL) {
+    static uint32_t tap_deadline = 0;
+    if (record->event.pressed) {  // On pressed.
+      tap_deadline = timer_read32() + 200;  // Set 200 ms tap deadline.
+      layer_on(LTOSLL_MO_LAYER);
+      ltosl_state = 1;  // Set undetermined state.
+    } else {  // On release.
+      layer_off(LTOSLL_MO_LAYER);
+      if (ltosl_state && !timer_expired32(timer_read32(), tap_deadline)) {
+        // LTOSLL was released without pressing another key within 200 ms.
+        layer_on(LTOSLL_OSL_LAYER);
+        ltosl_state = 2;  // Acting like OSL.
+      }
+    }
+  return false;
   }
 
   if (!process_select_word(keycode, record, SELWRD)) { return false; }
@@ -111,8 +141,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 void post_process_record_user(uint16_t keycode, keyrecord_t* record) {
   // Turn off the layer if another key is pressed while acting like OSL. The
   // `(ltosl_state >>= 1)` both tests that state = 2 and shifts it toward zero.
-  if (keycode != LTOSL && (ltosl_state >>= 1)) {
-    layer_off(LTOSL_OSL_LAYER);
+  if (keycode != LTOSLR && (ltosl_state >>= 1)) {
+    layer_off(LTOSLR_OSL_LAYER);
+  }
+  if (keycode != LTOSLL && (ltosl_state >>= 1)) {
+    layer_off(LTOSLL_OSL_LAYER);
   }
 }
 
@@ -132,7 +165,11 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             rgblight_sethsv_noeeprom(HSV_BLUE);
             rgblight_mode_noeeprom(1);
             break;
-        case _UTILITY:
+        case _UTILITY_RIGHT:
+            rgblight_sethsv_noeeprom(HSV_GREEN);
+            rgblight_mode_noeeprom(1);
+            break;
+        case _UTILITY_LEFT:
             rgblight_sethsv_noeeprom(HSV_GREEN);
             rgblight_mode_noeeprom(1);
             break;

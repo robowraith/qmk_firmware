7e60b98c4ac8b12e77e819c7ec2dc4fcdc1c9e0c
Joachim Hoss <joachim.hoss@42he.com>
2022-06-30 19:32:48 +0200

    First ideas

--- keyboards/momoka_ergo/keymaps/robowraith/keymap.c
+++ keyboards/momoka_ergo/keymaps/robowraith/keymap.c
@@ -18,196 +18,82 @@
 #include "features/select_word.h"
 extern rgblight_config_t rgblight_config;
 
-// Home-Row-Mod-Keys for Bone
-enum custom_keycodes {
-    BO_H = LGUI_T(DE_H),
-    BO_A = LALT_T(DE_A),
-    BO_E = LCTL_T(DE_E),
-    BO_I = LSFT_T(DE_I),
-    BO_T = RSFT_T(DE_T),
-    BO_R = RCTL_T(DE_R),
-    BO_N = RALT_T(DE_N),
-    BO_S = RGUI_T(DE_S),
-    SELWRD = SAFE_RANGE,
-    LTOSLR,
-    LTOSLL
-};
-
 enum layer_names {
-    _KOY,
+    _AEIOU,
     _SYMBOLS,
     _KEYPAD,
     _NAVIGATION,
     _GAMING,
 };
 
+// Home-Row-Mod-Keys for Bone
+enum custom_keycodes {
+    JK_U = LGUI_T(DE_U),
+    JK_O = LALT_T(DE_O),
+    JK_I = LCTL_T(DE_I),
+    JK_E = LSFT_T(DE_E),
+    JK_T = RSFT_T(DE_T),
+    JK_R = RCTL_T(DE_R),
+    JK_N = RALT_T(DE_N),
+    JK_S = RGUI_T(DE_S),
+    JK_ESC = LT(_SYMBOLS, KC_ESC),
+    JK_BSPC = LT(_SYMBOLS, KC_BSPC),
+    JK_DEL = LT(_SYMBOLS, KC_DEL),
+    JK_TAB = LT(_SYMBOLS, KC_TAB),
+    JK_ENT = LT(_SYMBOLS, KC_ENT),
+    JK_SPC = LT(_SYMBOLS, KC_SPC),
+    SELWRD = SAFE_RANGE
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_KOY] = LAYOUT(
-        KC_CUT,   DE_1,       DE_2,      DE_3,      DE_4,      DE_5,            /**/         DE_6,      DE_7,      DE_8,      DE_9,       DE_0,      TO(4),
-        KC_COPY,  DE_K,       DE_DOT,    DE_O,      DE_COMM,   DE_Y,            /**/         DE_V,      DE_G,      DE_C,      DE_L,       DE_SS,     DE_Z,
-        KC_PASTE, BO_H,       BO_A,      BO_E,      BO_I,      DE_U,            /**/         DE_D,      BO_T,      BO_R,      BO_N,       BO_S,      DE_F,
-        CAPSWRD,  DE_X,       DE_Q,      DE_ADIA,   DE_UDIA,   DE_ODIA,         /**/         DE_B,      DE_P,      DE_W,      DE_M,       DE_J,      KC_NO,
-        SELWRD,   KC_BRID,    KC_BRIU,   MO(3),     LTOSLR,                     /**/                    LTOSLL,    MO(2),     KC_VOLD,    KC_VOLU,   KC_MUTE,
-                                                               KC_NO,   KC_PSCR,/**/KC_RGUI, KC_NO,
-                                                                        KC_NO,  /**/KC_APP,
-                                                    KC_BSPC,   KC_DEL,  KC_ESC, /**/KC_TAB,  KC_ENT,    KC_SPC
+    [_AEIOU] = LAYOUT(
+        XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         /**/         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,
+        XXXXXXX,  DE_UDIA,    DE_ODIA,   DE_K,      DE_Y,      DE_ADIA,         /**/         DE_V,      DE_G,      DE_C,      DE_L,       DE_W,       XXXXXXX,
+        XXXXXXX,  JK_U,       JK_O,      JK_I,      JK_E,      DE_A,            /**/         DE_D,      JK_T,      JK_R,      JK_N,       JK_S,       XXXXXXX,
+        XXXXXXX,  DE_Z,       DE_Q,      DE_M,      DE_H,      DE_X,            /**/         DE_J,      DE_P,      DE_B,      DE_F,       DE_SS,      RESET,
+        XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX,   JK_ESC,                     /**/                    JK_TAB,   XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,
+                                                               XXXXXXX, XXXXXXX,/**/XXXXXXX, XXXXXXX,
+                                                                        XXXXXXX,/**/XXXXXXX,
+                                                    JK_BSPC,   JK_DEL,  XXXXXXX,/**/XXXXXXX, JK_ENT,    JK_SPC
     ),
     [_SYMBOLS] = LAYOUT(
-        KC_F1,    KC_F2,      KC_F3,     KC_F4,     KC_F5,     KC_F6,           /**/         KC_F7,     KC_F8,     KC_F9,     KC_F10,     KC_F11,     KC_F12,
-        KC_NO,    DE_DEG,     DE_UNDS,   DE_LBRC,   DE_RBRC,   DE_CIRC,         /**/         DE_EXLM,   DE_LABK,   DE_RABK,   DE_EQL,     DE_AMPR,    DE_EURO,
-        KC_NO,    DE_BSLS,    DE_SLSH,   DE_LCBR,   DE_RCBR,   DE_ASTR,         /**/         DE_QUES,   DE_LPRN,   DE_RPRN,   DE_MINS,    DE_COLN,    DE_AT,
-        KC_NO,    DE_HASH,    DE_DLR,    DE_PIPE,   DE_TILD,   DE_GRV,          /**/         DE_PLUS,   DE_PERC,   DE_DQUO,   DE_QUOT,    DE_SCLN,    KC_NO,
-        KC_NO,    KC_NO,      KC_NO,     KC_NO,     KC_NO,                      /**/                    KC_NO,     KC_NO,     KC_NO,      KC_NO,      KC_NO,
-                                                               KC_LCTL, KC_LALT,/**/KC_RGUI, KC_RCTL,
-                                                                        KC_HOME,/**/KC_PGUP,
-                                                    KC_BSPC,   KC_DEL,  KC_ESC, /**/KC_TAB,  KC_ENT,    KC_SPC
+        XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         /**/         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,
+        XXXXXXX,  KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         DE_LPRN,   DE_RPRN,   DE_MINS,   DE_UNDS,    DE_PLUS,    XXXXXXX,
+        XXXXXXX,  KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         DE_AT,     DE_DOT,    DE_COMM,   DE_EXLM,    DE_QUES,    XXXXXXX,
+        XXXXXXX,  KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         DE_COLN,   DE_SCLN,   DE_DQUO,   DE_QUOT,    DE_GRV,     RESET,
+        XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX,   XXXXXXX,                    /**/                    XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,
+                                                               XXXXXXX, XXXXXXX,/**/XXXXXXX, XXXXXXX,
+                                                                        XXXXXXX,/**/XXXXXXX,
+                                                    KC_NO,     KC_NO,   KC_NO,  /**/KC_NO,   KC_NO,    KC_NO
     ),
     [_KEYPAD] = LAYOUT(
-        KC_NO,    KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      KC_NO,
-        KC_NO,    KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NUM,    KC_KP_7,   KC_KP_8,   KC_KP_9,    KC_PPLS,    KC_PAST,
-        KC_NO,    KC_LGUI,    KC_LALT,   KC_LCTL,   KC_LSFT,   KC_NO,           /**/         KC_NO,     KC_KP_4,   KC_KP_5,   KC_KP_6,    KC_PMNS,    KC_PSLS,
-        KC_NO,    KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_KP_1,   KC_KP_2,   KC_KP_3,    KC_NO,      RESET,
-        TO(0),    KC_NO,      KC_NO,     KC_NO,     KC_NO,                      /**/                    KC_KP_0,    KC_NO,     KC_MPRV,    KC_MNXT,    KC_MPLY,
-                                                               KC_NO,   KC_NO,  /**/KC_NO,   KC_NO,
-                                                                        KC_NO,  /**/KC_NO,
-                                                    KC_NO,     KC_NO,   KC_NO,  /**/KC_NO,   KC_ENT,    KC_SPC
+        XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         /**/         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,
+        XXXXXXX,  KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      XXXXXXX,
+        XXXXXXX,  KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      XXXXXXX,
+        XXXXXXX,  KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      RESET,
+        XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX,   XXXXXXX,                    /**/                    XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,
+                                                               XXXXXXX, XXXXXXX,/**/XXXXXXX, XXXXXXX,
+                                                                        XXXXXXX,/**/XXXXXXX,
+                                                    KC_NO,     KC_NO,   KC_NO,  /**/KC_NO,   KC_NO,    KC_NO
     ),
     [_NAVIGATION] = LAYOUT(
-        KC_NO,    KC_NO,      KC_NO,     KC_PGUP,   KC_NO,     KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      KC_NO,
-        KC_NO,    KC_NO,      KC_UNDO,   KC_UP,     S(KC_UNDO),KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      KC_NO,
-        KC_CAPS,  KC_HOME,    KC_LEFT,   KC_DOWN,   KC_RIGHT,  KC_END,          /**/         KC_NO,     KC_RSFT,   KC_RCTL,   KC_RALT,    KC_RGUI,    KC_NO,
-        RESET,    KC_NO,      KC_NO,     KC_PGDN,   KC_NO,     KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      KC_NO,
-        TO(0),    KC_NO,      KC_NO,     KC_NO,     KC_NO,                      /**/                    KC_NO,     KC_NO,     KC_NO,      KC_NO,      KC_NO,
-                                                               KC_NO,   KC_NO,  /**/KC_NO,   KC_NO,
-                                                                        KC_NO,  /**/KC_NO,
-                                                    KC_BSPC,   KC_DEL,  KC_NO,  /**/KC_NO,   KC_NO,     KC_NO
+        XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         /**/         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,
+        XXXXXXX,  KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      XXXXXXX,
+        XXXXXXX,  KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      XXXXXXX,
+        XXXXXXX,  KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      RESET,
+        XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX,   XXXXXXX,                    /**/                    XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,
+                                                               XXXXXXX, XXXXXXX,/**/XXXXXXX, XXXXXXX,
+                                                                        XXXXXXX,/**/XXXXXXX,
+                                                    KC_NO,     KC_NO,   KC_NO,  /**/KC_NO,   KC_NO,    KC_NO
     ),
     [_GAMING] = LAYOUT(
-        KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,            /**/         KC_F1,     KC_F2,     KC_F3,     KC_F4,      KC_F5,      KC_F6,
-        KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,            /**/         KC_Y,      KC_KP_7,   KC_KP_8,   KC_KP_9,    KC_PAST,    KC_F7,
-        KC_CAPS,   KC_A,      KC_S,      KC_D,      KC_F,      KC_G,            /**/         KC_H,      KC_KP_4,   KC_KP_5,   KC_KP_6,    KC_PSLS,    KC_F8,
-        KC_LSFT,   KC_C,      KC_V,      KC_B,      KC_N,      KC_Z,            /**/         KC_X,      KC_KP_1,   KC_KP_2,   KC_KP_3,    KC_PMNS,    KC_F9,
-        KC_LCTL,   KC_I,      KC_O,      KC_P,      KC_J,                       /**/                    KC_U,      KC_P,      KC_F12,     KC_F11,     KC_F10,
-                                                               KC_L,    KC_M,   /**/RGB_MOD, RGB_TOG,
-                                                                        TO(0),  /**/KC_VOLU,
-                                                    KC_SPC,    KC_ENT,  KC_ESC, /**/KC_VOLD, KC_MUTE,   KC_KP_0
+        XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         /**/         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,
+        XXXXXXX,  KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      XXXXXXX,
+        XXXXXXX,  KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      XXXXXXX,
+        XXXXXXX,  KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,           /**/         KC_NO,     KC_NO,     KC_NO,     KC_NO,      KC_NO,      RESET,
+        XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX,   XXXXXXX,                    /**/                    XXXXXXX,   XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,
+                                                               XXXXXXX, XXXXXXX,/**/XXXXXXX, XXXXXXX,
+                                                                        XXXXXXX,/**/XXXXXXX,
+                                                    KC_NO,     KC_NO,   KC_NO,  /**/KC_NO,   KC_NO,    KC_NO
     )
-};
-
-static uint8_t ltosl_state = 0;
-#define LTOSLR_MO_LAYER 2   // Layer to activate when holding.
-#define LTOSLR_OSL_LAYER 1  // Layer to activate as an OSL when tapped.
-#define LTOSLL_MO_LAYER 3   // Layer to activate when holding.
-#define LTOSLL_OSL_LAYER 1  // Layer to activate as an OSL when tapped.
-bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-  if (keycode == LTOSLR) {
-    static uint32_t tap_deadline = 0;
-    if (record->event.pressed) {  // On pressed.
-      tap_deadline = timer_read32() + 200;  // Set 200 ms tap deadline.
-      layer_on(LTOSLR_MO_LAYER);
-      ltosl_state = 1;  // Set undetermined state.
-    } else {  // On release.
-      layer_off(LTOSLR_MO_LAYER);
-      if (ltosl_state && !timer_expired32(timer_read32(), tap_deadline)) {
-        // LTOSLR was released without pressing another key within 200 ms.
-        layer_on(LTOSLR_OSL_LAYER);
-        ltosl_state = 2;  // Acting like OSL.
-      }
-    }
-    return false;
-  }
-  if (keycode == LTOSLL) {
-    static uint32_t tap_deadline = 0;
-    if (record->event.pressed) {  // On pressed.
-      tap_deadline = timer_read32() + 200;  // Set 200 ms tap deadline.
-      layer_on(LTOSLL_MO_LAYER);
-      ltosl_state = 1;  // Set undetermined state.
-    } else {  // On release.
-      layer_off(LTOSLL_MO_LAYER);
-      if (ltosl_state && !timer_expired32(timer_read32(), tap_deadline)) {
-        // LTOSLL was released without pressing another key within 200 ms.
-        layer_on(LTOSLL_OSL_LAYER);
-        ltosl_state = 2;  // Acting like OSL.
-      }
-    }
-  return false;
-  }
-
-  if (!process_select_word(keycode, record, SELWRD)) { return false; }
-
-  return true;
-}
-
-void post_process_record_user(uint16_t keycode, keyrecord_t* record) {
-  // Turn off the layer if another key is pressed while acting like OSL. The
-  // `(ltosl_state >>= 1)` both tests that state = 2 and shifts it toward zero.
-  if (keycode != LTOSLR && (ltosl_state >>= 1)) {
-    layer_off(LTOSLR_OSL_LAYER);
-  }
-  if (keycode != LTOSLL && (ltosl_state >>= 1)) {
-    layer_off(LTOSLL_OSL_LAYER);
-  }
-}
-
-void keyboard_post_init_user(void) {
-    rgblight_mode(22);
-    rgblight_sethsv(HSV_RED);
-   };
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    uint8_t layer = biton32(state);
-    switch (layer) {
-        case _KOY:
-            rgblight_sethsv_noeeprom(HSV_WHITE);
-            rgblight_mode_noeeprom(1);
-            break;
-        case _SYMBOLS:
-            rgblight_sethsv_noeeprom(HSV_BLUE);
-            rgblight_mode_noeeprom(1);
-            break;
-        case _KEYPAD:
-            rgblight_sethsv_noeeprom(HSV_GREEN);
-            rgblight_mode_noeeprom(1);
-            break;
-        case _NAVIGATION:
-            rgblight_sethsv_noeeprom(HSV_GREEN);
-            rgblight_mode_noeeprom(1);
-            break;
-         case _GAMING:
-            rgblight_sethsv_noeeprom(HSV_CYAN);
-            rgblight_mode_noeeprom(14);
-            break;
-    }
-    return state;
-   };
-
-void caps_word_set_user(bool active) {
-    if (active) {
-        rgblight_sethsv_noeeprom(HSV_RED);
-        rgblight_mode_noeeprom(5);
-    } else {
-        rgblight_mode_noeeprom(1);
-        rgblight_sethsv_noeeprom(HSV_WHITE);
-    }
-};
-
-bool caps_word_press_user(uint16_t keycode) {
-    switch (keycode) {
-        // Keycodes that continue Caps Word, with shift applied.
-        case KC_A ... KC_Z:
-        case KC_MINS:
-            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
-            return true;
-
-        // Keycodes that continue Caps Word, without shifting.
-        case KC_1 ... KC_0:
-        case KC_BSPC:
-        case KC_DEL:
-        case KC_UNDS:
-        case DE_UNDS:
-            return true;
-
-        default:
-            return false;  // Deactivate Caps Word.
-    }
 };

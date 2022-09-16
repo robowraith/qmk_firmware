94f06612b51bf0e5f62ef2a7f61bf54eb2202c71
Joachim Hoss <robowraith@gmail.com>
2022-09-16 13:17:40 +0200

    Decrease Tapping Term

--- keyboards/xbows/nature/keymaps/robowraith/config.h
+++ keyboards/xbows/nature/keymaps/robowraith/config.h
@@ -16,7 +16,7 @@
 #define LTO_ENABLE   TRUE
 
 #define TAPPING_TOGGLE 2
-#define TAPPING_TERM 200
+#define TAPPING_TERM 150
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT

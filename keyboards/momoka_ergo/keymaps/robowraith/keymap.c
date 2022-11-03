e2cfd39671044acf5c9bb8edad973e026dbde46e
Joachim Hoss <robowraith@gmail.com>
2022-11-03 23:03:33 +0100

    Add Printscreen key

--- keyboards/momoka_ergo/keymaps/robowraith/keymap.c
+++ keyboards/momoka_ergo/keymaps/robowraith/keymap.c
@@ -92,7 +92,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        KC_NO,      KC_NO,      TD(PREV),   TD(NEXT),   TD(VOLD),   TD(VOLU),   /**/    TD(DISM),   KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+        KC_PSCR,    KC_NO,      TD(PREV),   TD(NEXT),   TD(VOLD),   TD(VOLU),   /**/    TD(DISM),   KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
         KC_NO,      KC_NO,      DE_L,       DE_U,       DE_A,       DE_Q,       /**/    DE_W,       DE_B,       DE_D,       DE_G,       KC_NO,      KC_NO,
         DE_J,       RW_C,       RW_R,       RW_I,       RW_E,       DE_O,       /**/    DE_M,       RW_N,       RW_T,       RW_S,       RW_H,       DE_Y,
         KC_NO,      DE_V,       DE_X,       DE_UDIA,    DE_ADIA,    DE_ODIA,    /**/    DE_P,       DE_F,       DE_Z,       DE_SS,      DE_K,       KC_NO,

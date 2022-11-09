f0dfe76b350a764d762bac690e65048033233b2f
Joachim Hoss <joachim.hoss@42he.com>
2022-11-09 18:18:25 +0100

    Move some keys around

--- keyboards/momoka_ergo/keymaps/robowraith/keymap.c
+++ keyboards/momoka_ergo/keymaps/robowraith/keymap.c
@@ -96,14 +96,14 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        KC_PSCR,    KC_NO,      TD(PREV),   TD(NEXT),   TD(VOLD),   TD(VOLU),   /**/    TD(DISM),   KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
-        KC_NO,      KC_NO,      DE_L,       DE_U,       DE_A,       DE_Q,       /**/    DE_W,       DE_B,       DE_D,       DE_G,       KC_NO,      KC_NO,
+        TD(VOLD),   TD(VOLU),   KC_NO,      KC_NO,      KC_NO,      KC_PSCR,    /**/    TD(DISM),   KC_COPY,    KC_PASTE,   KC_CUT,     KC_NO,      KC_NO,
+        TD(PREV)    TD(NEXT)    DE_L,       DE_U,       DE_A,       DE_Q,       /**/    DE_W,       DE_B,       DE_D,       DE_G,       KC_NO,      KC_NO,
         DE_J,       RW_C,       RW_R,       RW_I,       RW_E,       DE_O,       /**/    DE_M,       RW_N,       RW_T,       RW_S,       RW_H,       DE_Y,
         KC_NO,      DE_V,       DE_X,       DE_UDIA,    DE_ADIA,    DE_ODIA,    /**/    DE_P,       DE_F,       DE_Z,       DE_SS,      DE_K,       KC_NO,
         XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_ESC,                 /**/                KC_TAB,     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
                                                         XXXXXXX,    XXXXXXX,    /**/    XXXXXXX,    XXXXXXX,
-                                                                    KC_NO,      /**/    KC_NO,
-                                            KC_BSPC,    LTOSLL,     KC_NO,      /**/    KC_NO,      LTOSLR,     KC_SPC
+                                                                    KC_NO,      /**/    XXXXXXX,
+                                            KC_BSPC,    LTOSLL,     KC_NO,      /**/    XXXXXXX,    LTOSLR,     KC_SPC
     ),
     [_SYMBOLS] = LAYOUT(
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      /**/    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
